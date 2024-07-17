9ddf6dc4c3b370437cafafe581938875399234ee
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-17 02:36:37 -0700

    backend setup with add middlewares for handling errors

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDatabase from "./db/Database.js";
+import dotenv from "dotenv";
+import errorHandler from "./middleware/error.js";
+
+// config
+const app = express();
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({
+    path: "config/.env",
+  });
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+connectDatabase();
+
+// import routes
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// it's for ErrorHandling
+app.use(errorHandler);
+
+// create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
