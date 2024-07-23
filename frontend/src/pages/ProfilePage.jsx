7ca9d384578e58b56f0e2ca9084669b5a6735b01
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-23 03:00:11 -0700

    make profile page private

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import ProfileContent from "../components/ProfilePageComponents/ProfileContent";
 import ProfileSidebar from "../components/ProfilePageComponents/ProfileSidebar";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
   const [active, setActive] = useState(1);
+  const { isAuthenticated } = useSelector((state) => state.user);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [navigate, isAuthenticated]);
+
   return (
     <div>
       <Header />
-      <div className="section bg-[#f5f5f5] py-8 flex">
-        <div className="max-w-[270px] w-full">
+      <div className="section flex bg-[#f5f5f5] py-8">
+        <div className="w-full max-w-[270px]">
           <ProfileSidebar active={active} setActive={setActive} />
         </div>
         <ProfileContent active={active} />
