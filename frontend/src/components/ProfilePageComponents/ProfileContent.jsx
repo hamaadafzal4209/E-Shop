3b284912c645bb180cc201989f82b09fde6a84fb
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-23 00:00:06 -0700

    complete track order component

--- frontend/src/components/ProfilePageComponents/ProfileContent.jsx
+++ frontend/src/components/ProfilePageComponents/ProfileContent.jsx
@@ -1,16 +1,22 @@
 import AllOrders from "./AllOrders";
 import AllRefundOrders from "./AllRefundOrders";
 import Profile from "./Profile";
+import TrackOrders from "./TrackOrders";
 
 function ProfileContent({ active }) {
   return (
     <div className="w-full">
       {/* profile page */}
       {active === 1 && <Profile />}
+
       {/* ordres page */}
       {active === 2 && <AllOrders />}
+
       {/* refund ordres page */}
       {active === 3 && <AllRefundOrders />}
+
+      {/* track ordres page */}
+      {active === 5 && <TrackOrders />}
     </div>
   );
 }
