ee7e94dbf9fdeeccc50e80f7317b47928780615f
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-22 23:49:23 -0700

    complete all refund order component

---
+++ frontend/src/components/ProfilePageComponents/AllRefundOrders.jsx
@@ -0,0 +1,89 @@
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { DataGrid } from "@mui/x-data-grid";
+import { Button } from "@mui/material";
+
+function AllRefundOrders() {
+  const orders = [
+    {
+      _id: "3872738263872y3x7nnx72362",
+      orderItems: [
+        {
+          name: "Iphone 14 pro max",
+        },
+      ],
+      totalPrice: 150,
+      orderStatus: "Processing",
+    },
+  ];
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "Order ID",
+      minWidth: 150,
+      flex: 0.7,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.value === "Delivered" ? "greenColor" : "redColor";
+      },
+    },
+    {
+      field: "itemQty",
+      headerName: "Item Qty",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
+      flex: 0.7,
+    },
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "",
+      type: "number",
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <Link to={`/order/${params.id}`}>
+            <Button>
+              <AiOutlineArrowRight size={20} />
+            </Button>
+          </Link>
+        );
+      },
+    },
+  ];
+
+  const rows = orders.map((item) => ({
+    id: item._id,
+    itemQty: item.orderItems.length,
+    total: "USD " + item.totalPrice,
+    status: item.orderStatus,
+  }));
+
+  return (
+    <div className="pl-6 pt-1 font-semibold">
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+        autoHeight
+        disableRowSelectionOnClick
+      />
+    </div>
+  );
+}
+
+export default AllRefundOrders;
--- frontend/src/components/ProfilePageComponents/ProfileContent.jsx
+++ frontend/src/components/ProfilePageComponents/ProfileContent.jsx
@@ -1,17 +1,16 @@
 import AllOrders from "./AllOrders";
+import AllRefundOrders from "./AllRefundOrders";
 import Profile from "./Profile";
 
 function ProfileContent({ active }) {
-
   return (
     <div className="w-full">
       {/* profile page */}
-      {active === 1 && (
-        <Profile/>
-      )}
-      {active === 2 && (
-        <AllOrders/>
-      )}
+      {active === 1 && <Profile />}
+      {/* ordres page */}
+      {active === 2 && <AllOrders />}
+      {/* refund ordres page */}
+      {active === 3 && <AllRefundOrders />}
     </div>
   );
 }
