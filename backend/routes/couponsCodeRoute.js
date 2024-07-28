b156f07c9b0ed981d21fc455de7bf7a6b53ef118
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-27 23:56:53 -0700

    commit

--- backend/routes/couponsCodeRoute.js
+++ backend/routes/couponsCodeRoute.js
@@ -1,11 +1,15 @@
-import express from 'express';
-import { isSeller } from '../middleware/auth.js';
-import { createCoupon,getCoupons,deleteCoupon } from '../controller/couponCodeController.js';
+import express from "express";
+import { isSeller } from "../middleware/auth.js";
+import {
+  createCoupon,
+  getCoupons,
+  deleteCoupon,
+} from "../controller/couponCodeController.js";
 
 const couponsCodeRouter = express.Router();
 
-couponsCodeRouter.post("/create-coupon",isSeller, createCoupon)
-couponsCodeRouter.get("/get-coupons/:id",isSeller, getCoupons)
-couponsCodeRouter.delete("/delete-coupon/:id",isSeller, deleteCoupon)
+couponsCodeRouter.post("/create-coupon", isSeller, createCoupon);
+couponsCodeRouter.get("/get-coupons/:id", isSeller, getCoupons);
+couponsCodeRouter.delete("/delete-coupon/:id", isSeller, deleteCoupon);
 
 export default couponsCodeRouter;
