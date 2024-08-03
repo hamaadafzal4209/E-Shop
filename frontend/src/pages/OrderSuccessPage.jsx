020b5fd7c90e23aef2725d8348e8363874e25cf0
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-03 00:06:14 -0700

    commit

--- frontend/src/pages/OrderSuccessPage.jsx
+++ frontend/src/pages/OrderSuccessPage.jsx
@@ -1,37 +1,37 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Lottie from 'react-lottie';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Lottie from "react-lottie";
 import animationData from "../Assests/animations/107043-success.json";
 
 function OrderSuccessPage() {
   return (
     <div>
-      <Header/>
-      <Success/>
-      <Footer/>
+      <Header />
+      <Success />
+      <Footer />
     </div>
-  )
+  );
 }
 
 const Success = () => {
-    const defaultOptions = {
-      loop: false,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice",
-      },
-    };
-    return (
-      <div>
-        <Lottie options={defaultOptions} width={300} height={300} />
-        <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
-          Your order is successful 😍
-        </h5>
-        <br />
-        <br />
-      </div>
-    );
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
+  return (
+    <div>
+      <Lottie options={defaultOptions} width={300} height={300} />
+      <h5 className="mb-14 text-center text-[25px] text-[#000000a1]">
+        Your order is successful 😍
+      </h5>
+      <br />
+      <br />
+    </div>
+  );
+};
 
-export default OrderSuccessPage
+export default OrderSuccessPage;
