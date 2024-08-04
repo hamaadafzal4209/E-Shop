947223dcdadd935641daf391819d9501a6eac31d
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-04 00:44:05 -0700

    create shop all orders component

--- frontend/src/components/SellerComponent/ShopDashBoard/ShopAllOrders.jsx
+++ frontend/src/components/SellerComponent/ShopDashBoard/ShopAllOrders.jsx
@@ -1,111 +1,81 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  deleteShopProducts,
-  getAllShopProducts,
-} from "../../../redux/actions/product";
-import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { AiOutlineArrowRight } from "react-icons/ai";
 import { DataGrid } from "@mui/x-data-grid";
 import { Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import Loader from "../../Loader";
+import { getAllOrdersOfShop } from "../../../redux/actions/order";
 
 function ShopAllOrders() {
-  const { products, isLoading } = useSelector((state) => state.products);
+  const { orders, isLoading } = useSelector((state) => state.orders);
   const { seller } = useSelector((state) => state.seller);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (seller?._id) {
-      dispatch(getAllShopProducts(seller._id));
+      dispatch(getAllOrdersOfShop(seller._id));
     }
   }, [dispatch, seller._id]);
 
-  const handleDelete = (id) => {
-    dispatch(deleteShopProducts(id));
-    window.location.reload();
-  };
-
   const columns = [
-    { field: "id", headerName: "Product Id", minWidth: 150, flex: 0.8, align: 'left', headerAlign: 'left' },
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
     {
-      field: "name",
-      headerName: "Name",
-      minWidth: 180,
-      flex: 1.5,
-      align: 'left',
-      headerAlign: 'left',
+      field: "status",
+      headerName: "Status",
+      minWidth: 130,
+      flex: 0.7,
+      cellClassName: (params) => {
+        return params.value === "Delivered" ? "greenColor" : "redColor";
+      },
     },
     {
-      field: "price",
-      headerName: "Price",
-      minWidth: 120,
-      flex: 0.8,
-      align: 'center',
-      headerAlign: 'center',
-    },
-    {
-      field: "stock",
-      headerName: "Stock",
-      type: "number",
-      minWidth: 100,
-      flex: 0.6,
-      align: 'center',
-      headerAlign: 'center',
-    },
-    {
-      field: "sold",
-      headerName: "Sold out",
+      field: "itemsQty",
+      headerName: "Items Qty",
       type: "number",
       minWidth: 130,
       flex: 0.7,
-      align: 'center',
-      headerAlign: 'center',
     },
+
     {
-      field: "preview",
+      field: "total",
+      headerName: "Total",
+      type: "number",
+      minWidth: 130,
       flex: 0.8,
-      minWidth: 100,
-      headerName: "Preview",
+    },
+
+    {
+      field: " ",
+      flex: 1,
+      minWidth: 150,
+      headerName: "Order Detail",
+      type: "number",
       sortable: false,
-      align: 'center',
-      headerAlign: 'center',
       renderCell: (params) => {
         return (
-          <Link to={`/product/${encodeURIComponent(params.row.name)}`}>
+          <Link to={`/order/${params.id}`}>
             <Button>
-              <AiOutlineEye size={20} />
+              <AiOutlineArrowRight size={20} />
             </Button>
           </Link>
         );
       },
     },
-    {
-      field: "delete",
-      flex: 0.8,
-      minWidth: 120,
-      headerName: "Delete",
-      sortable: false,
-      align: 'center',
-      headerAlign: 'center',
-      renderCell: (params) => {
-        return (
-          <Button onClick={() => handleDelete(params.row.id)}>
-            <AiOutlineDelete size={20} />
-          </Button>
-        );
-      },
-    },
   ];
 
-  const rows =
-    products?.map((item) => ({
-      id: item._id,
-      name: item.name,
-      price: `US$${item.discountPrice}`,
-      stock: item.stock,
-      sold: 10,
-    })) || [];
+  const rows = [];
+
+  orders &&
+    orders.forEach((item) => {
+      rows.push({
+        id: item._id,
+        itemsQty: item.cart.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status,
+      });
+    });
 
   return isLoading ? (
     <Loader />
