ee4988eb9206ab8f8d8d29a8074cda71c0edd866
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-04 05:07:27 -0700

    modify updateOrderStatus api route

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -2,6 +2,7 @@ import orderModel from "../model/orderModel.js";
 import productModel from "../model/productModel.js";
 import catchAsyncErrors from "../middleware/catchAsyncErrors.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
+import shopModel from "../model/shopModel.js";
 
 // create new order
 export const createOrder = catchAsyncErrors(async (req, res, next) => {
@@ -120,7 +121,7 @@ export const updateOrderStatus = catchAsyncErrors(async (req, res, next) => {
     }
 
     async function updateSellerInfo(amount) {
-      const seller = await Shop.findById(req.seller.id);
+      const seller = await shopModel.findById(req.seller.id);
 
       seller.availableBalance = amount;
 
