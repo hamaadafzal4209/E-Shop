d647b60038333038dfbb5f306457e0480c3a1008
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-05 03:46:54 -0700

    modify track order component

--- frontend/src/components/ProfilePageComponents/TrackOrders.jsx
+++ frontend/src/components/ProfilePageComponents/TrackOrders.jsx
@@ -1,85 +1,95 @@
-import { MdOutlineTrackChanges } from "react-icons/md";
+import { MdTrackChanges } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { DataGrid } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
+import { useEffect } from "react";
+import { getAllOrdersOfUser } from "../../redux/actions/order";
+import { useDispatch, useSelector } from "react-redux";
 
 function TrackOrders() {
-  const orders = [
-    {
-      _id: "3872738263872y3x7nnx72362",
-      orderItems: [
-        {
-          name: "Iphone 14 pro max",
-        },
-      ],
-      totalPrice: 150,
-      orderStatus: "Processing",
-    },
-  ];
+  const { user } = useSelector((state) => state.user);
+  const { orders } = useSelector((state) => state.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllOrdersOfUser(user._id));
+  }, [dispatch, user._id]);
 
   const columns = [
-    {
-      field: "id",
-      headerName: "Order ID",
-      minWidth: 150,
-      flex: 0.7,
-    },
+    { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+
     {
       field: "status",
       headerName: "Status",
       minWidth: 130,
       flex: 0.7,
       cellClassName: (params) => {
-        return params.value === "Delivered" ? "greenColor" : "redColor";
+        return params.row.status === "Delivered" ? "greenColor" : "redColor";
       },
     },
     {
-      field: "itemQty",
-      headerName: "Item Qty",
+      field: "itemsQty",
+      headerName: "Items Qty",
       type: "number",
       minWidth: 130,
       flex: 0.7,
     },
+
     {
       field: "total",
       headerName: "Total",
       type: "number",
       minWidth: 130,
-      flex: 0.7,
+      flex: 0.8,
     },
+
     {
       field: " ",
       flex: 1,
-      minWidth: 130,
+      minWidth: 150,
       headerName: "",
       type: "number",
       sortable: false,
       renderCell: (params) => {
         return (
-          <Link to={`/order/${params.id}`}>
-            <Button>
-              <MdOutlineTrackChanges size={20} color="black" />
-            </Button>
-          </Link>
+          <>
+            <Link to={`/user/track/order/${params.id}`}>
+              <Button>
+                <MdTrackChanges size={20} />
+              </Button>
+            </Link>
+          </>
         );
       },
     },
   ];
 
-  const rows = orders.map((item) => ({
-    id: item._id,
-    itemQty: item.orderItems.length,
-    total: "USD " + item.totalPrice,
-    status: item.orderStatus,
-  }));
+  const rows = [];
+
+  orders &&
+    orders.forEach((item) => {
+      rows.push({
+        id: item._id,
+        itemsQty: item.cart.length,
+        total: "US$ " + item.totalPrice,
+        status: item.status,
+      });
+    });
 
   return (
     <div className="pl-6 pt-1 font-semibold">
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={10}
-        autoHeight
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
         disableRowSelectionOnClick
       />
     </div>
